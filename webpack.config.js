609d7858319ca2c39233c289cd104360028bbfc5
Nikita Zhulev <Nikita_Zhulev@epam.com>
2022-10-27 12:57:53 +0300

    add stylelint

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,14 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
 };
